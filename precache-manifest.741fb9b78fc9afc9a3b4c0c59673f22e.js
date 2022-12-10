7a7e0307c47b5d6e12b60f9b9b74afac0319550d
Joe Schubert <joseph.schubert@gmail.com>
2022-12-10 07:11:07 +0000

    Updates

---
+++ precache-manifest.741fb9b78fc9afc9a3b4c0c59673f22e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad70fcbf721565ad3282c37d07d39bf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ca834032ebd62b94e05",
+    "url": "/static/css/3.6e95b680.chunk.css"
+  },
+  {
+    "revision": "51880040503d322b0892",
+    "url": "/static/css/4.f0b7a320.chunk.css"
+  },
+  {
+    "revision": "6de8c7a81817bc8dd7dd",
+    "url": "/static/css/main.8e76c157.chunk.css"
+  },
+  {
+    "revision": "7c2ff0c344dbdb8a1c94",
+    "url": "/static/js/2.7e763fd5.chunk.js"
+  },
+  {
+    "revision": "1ca834032ebd62b94e05",
+    "url": "/static/js/3.6a27fcad.chunk.js"
+  },
+  {
+    "revision": "51880040503d322b0892",
+    "url": "/static/js/4.292b583c.chunk.js"
+  },
+  {
+    "revision": "6de8c7a81817bc8dd7dd",
+    "url": "/static/js/main.68598283.chunk.js"
+  },
+  {
+    "revision": "8b546881c87426d33a8b",
+    "url": "/static/js/runtime-main.a3f08a29.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "203bc9337ab2dafb5c2ca3a2c39ebf64",
+    "url": "/static/media/aws-clf.203bc933.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/static/media/developerActivity.a41f7848.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "b1ae2adc77bafd0ab3f670d9ab06b5ca",
+    "url": "/static/media/jsFramework.b1ae2adc.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/static/media/manOnTable.a15c230b.svg"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/static/media/skill.96c9d7a9.svg"
+  },
+  {
+    "revision": "7053bc2eb9c1ab682fe3c98494d861bc",
+    "url": "/static/media/uscg.7053bc2e.png"
+  }
+]);
